d4d5d2bc5ba2453faf31714954606983f2337d61
MADHANKUMAR98 <83238170+MADHANKUMAR98@users.noreply.github.com>
2023-07-26 11:33:40 +0530

    commit2

--- question1/src/node.jsx
+++ question1/src/node.jsx
@@ -2,19 +2,28 @@ import React, { useEffect, useState } from 'react';
 
 const PrimeNumbers = () => {
   const [primeNumbers, setPrimeNumbers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://20.244.56.144/numbers/primes')
       .then(response => response.json())
-      .then(data => setPrimeNumbers(data))
-      .catch(error => console.error('Fetch error:', error));
+      .then(data => {
+        setPrimeNumbers(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div>
       <h1>Prime Numbers</h1>
-      {primeNumbers.length === 0 ? (
-        <p>Loading...</p> // Placeholder while data is being fetched
+      {loading ? (
+        <p>Loading...</p>
+      ) : primeNumbers.length === 0 ? (
+        <p></p>
       ) : (
         <ul>
           {primeNumbers.map((number, index) => (
